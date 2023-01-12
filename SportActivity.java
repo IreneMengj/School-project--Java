021bad4ef1011b103b5de94d063211f02ee966b9
Irene <88880169+IreneMengj@users.noreply.github.com>
2023-01-12 14:23:49 +0000

    Add files via upload

---
+++ SportActivity.java
@@ -0,0 +1,43 @@
+
+/*
+ *SportActivity.java
+ *@author JiayuMeng
+ *13/11/2022
+ */
+public class SportActivity {
+    //declare variables
+    int duration;
+    String name;
+    String place;
+
+    //constructor
+    public SportActivity() {
+    }
+
+
+    // Getters & Setters
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+}
